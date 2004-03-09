4d2b4ffec5d4996e2932a3030577467ef7eed064
Jamie Wilkinson <jaq@spacepants.org>
2004-03-09 10:30:42 +0000

    ------------------------------------------------------------------------ r745 | jaq | 2004-01-06 12:02:58 +1100 (Tue, 06 Jan 2004) | 2 lines
    - Code reformatting, and copyright information.

--- filter.c
+++ filter.c
@@ -1,4 +1,21 @@
-/* $Id: filter.c,v 1.16 2002/11/08 01:18:30 matthew Exp $ */
+/* data structure building routines
+ *
+ * Copyright (c) 2002,2003 Matthew Kirkwood
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 #include "filter.h"
 
