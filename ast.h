a0cf4a9bbda9db7e702407098c9ea90afdeb90ec
Jamie Wilkinson <jaq@spacepants.org>
2004-03-09 08:34:19 +0000

    ------------------------------------------------------------------------ r588 | jaq | 2003-10-27 12:10:10 +1100 (Mon, 27 Oct 2003) | 3 lines
    - Updated AST to build complete syntax tree, and updated test code to
      traverse the AST.

--- ast.h
+++ ast.h
@@ -13,15 +13,11 @@ struct subrule_list_s {
 
 struct chaingroup_specifier_s {
     char * name;
-    struct subrule_list_s * subrule_list;
-};
-
-struct log_text_argument_s {
-    char * identifier;
+    struct subrule_list_s * list;
 };
 
 struct compound_specifier_s {
-    struct subrule_list_s * subrule_list;
+    struct subrule_list_s * list;
 };
 
 struct routing_specifier_s {
@@ -33,21 +29,21 @@ struct simple_icmptype_argument_s {
 };
 
 struct icmptype_argument_list_s {
-    struct icmptype_argument_list_s * icmptype_argument_list;
-    struct simple_icmptype_argument_s * simple_icmptype_argument;
+    struct icmptype_argument_list_s * list;
+    struct simple_icmptype_argument_s * arg;
 };
 
 struct compound_icmptype_argument_s {
-    struct icmptype_argument_list_s * icmptype_argument_list;
+    struct icmptype_argument_list_s * list;
 };
 
 struct icmptype_argument_s {
-    struct compound_icmptype_argument_s * compound_icmptype_argument;
-    struct simple_icmptype_argument_s * simple_icmptype_argument;
+    struct compound_icmptype_argument_s * compound;
+    struct simple_icmptype_argument_s * simple;
 };
 
 struct icmptype_specifier_s {
-    struct icmptype_argument_s * icmptype_argument;
+    struct icmptype_argument_s * arg;
 };
 
 struct simple_protocol_argument_s {
@@ -55,21 +51,21 @@ struct simple_protocol_argument_s {
 };
 
 struct protocol_argument_list_s {
-    struct protocol_argument_list_s * protocol_argument_list;
-    struct simple_protocol_argument_s * simple_protocol_argument;
+    struct protocol_argument_list_s * list;
+    struct simple_protocol_argument_s * arg;
 };
 
 struct compound_protocol_argument_s {
-    struct protocol_argument_list_s * protocol_argument_list;
+    struct protocol_argument_list_s * list;
 };
 
 struct protocol_argument_s {
-    struct compound_protocol_argument_s * compound_protocol_argument;
-    struct simple_protocol_argument_s * simple_protocol_argument;
+    struct compound_protocol_argument_s * compound;
+    struct simple_protocol_argument_s * simple;
 };
 
 struct protocol_specifier_s {
-    struct protocol_argument_s * protocol_argument;
+    struct protocol_argument_s * arg;
 };
 
 struct simple_port_argument_s {
@@ -78,22 +74,22 @@ struct simple_port_argument_s {
 };
 
 struct port_argument_list_s {
-    struct port_argument_list_s * port_argument_list;
-    struct simple_port_argument_s * simple_port_argument;
+    struct port_argument_list_s * list;
+    struct simple_port_argument_s * arg;
 };
 
 struct compound_port_argument_s {
-    struct port_argument_list_s * port_argument_list;
+    struct port_argument_list_s * list;
 };
 
 struct port_argument_s {
-    struct compound_port_argument_s * compound_port_argument;
-    struct simple_port_argument_s * simple_port_argument;
+    struct compound_port_argument_s * compound;
+    struct simple_port_argument_s * simple;
 };
 
 struct port_specifier_s {
     /* type */
-    struct port_argument_s * port_argument;
+    struct port_argument_s * arg;
 };
 
 struct simple_host_argument_s {
@@ -101,31 +97,35 @@ struct simple_host_argument_s {
 };
 
 struct host_argument_list_s {
-    struct host_argument_list_s * host_argument_list;
-    struct simple_host_argument_s * simple_host_argument;
+    struct host_argument_list_s * list;
+    struct simple_host_argument_s * arg;
 };
 
 struct compound_host_argument_s {
-    struct host_argument_list_s * host_argument_list;
+    struct host_argument_list_s * list;
 };
 
 struct host_argument_s {
-    struct compound_host_argument_s * compound_host_argument;
-    struct simple_host_argument_s * simple_host_argument;
+    struct compound_host_argument_s * compound;
+    struct simple_host_argument_s * simple;
 };
 
 struct host_specifier_s {
     /* type */
-    struct host_argument_s * host_argument;
+    struct host_argument_s * arg;
+};
+
+struct log_text_argument_s {
+    char * text;
 };
 
 struct log_target_specifier_s {
-    char * text;
+    struct log_text_argument_s * arg;
 };
 
 struct target_specifier_s {
     /* type */
-    struct log_target_specifier_s * log_target_specifier;
+    struct log_target_specifier_s * log;
 };
 
 struct simple_direction_argument_s {
@@ -133,57 +133,57 @@ struct simple_direction_argument_s {
 };
 
 struct direction_argument_list_s {
-    struct direction_argument_list_s * direction_argument_list;
-    struct simple_direction_argument_s * simple_direction_argument;
+    struct direction_argument_list_s * list;
+    struct simple_direction_argument_s * arg;
 };
 
 struct compound_direction_argument_s {
-    struct direction_argument_list_s * direction_argument_list;
+    struct direction_argument_list_s * list;
 };
 
 struct direction_argument_s {
-    struct compound_direction_argument_s * compound_direction_argument;
-    struct simple_direction_argument_s * simple_direction_argument;
+    struct compound_direction_argument_s * compound;
+    struct simple_direction_argument_s * simple;
 };
 
 struct direction_specifier_s {
     /* type */
-    struct direction_argument_s * direction_argument;
+    struct direction_argument_s * arg;
 };
 
 struct specifier_s {
-    struct compound_specifier_s * compound_specifier;
-    struct direction_specifier_s * direction_specifier;
-    struct target_specifier_s * target_specifier;
-    struct host_specifier_s * host_specifier;
-    struct port_specifier_s * port_specifier;
-    struct protocol_specifier_s * protocol_specifier;
-    struct icmptype_specifier_s * icmptype_specifier;
-    struct routing_specifier_s * routing_specifier;
-    struct chaingroup_specifier_s * chaingroup_specifier;
+    struct compound_specifier_s * compound;
+    struct direction_specifier_s * direction;
+    struct target_specifier_s * target;
+    struct host_specifier_s * host;
+    struct port_specifier_s * port;
+    struct protocol_specifier_s * protocol;
+    struct icmptype_specifier_s * icmptype;
+    struct routing_specifier_s * routing;
+    struct chaingroup_specifier_s * chaingroup;
 };
 
-struct negated_specifier {
+struct negated_specifier_s {
     /* type */
-    struct specifier_ * specifier;
+    struct specifier_s * spec;
 };
 
 struct specifier_list_s {
-    struct specifier_list_s * specifier_list;
-    struct negated_specifier_s * negated_specifier;
+    struct specifier_list_s * list;
+    struct negated_specifier_s * spec;
 };
 
 struct rule_s {
-    struct specifier_list_s * specifier_list;
+    struct specifier_list_s * list;
 };
 
 struct rule_list_s {
-    struct rule_list_s * rule_list;
+    struct rule_list_s * list;
     struct rule_s * rule;
 };
 
 struct ast_s {
-    struct rule_list_s * rule_list;
+    struct rule_list_s * list;
 };
 
 #endif /* __AST_H__ */
--- t/emit.c
+++ t/emit.c
@@ -7,24 +7,256 @@
 
 int yyparse(void *);
 
-void emit_rule(struct rule_s * rule) {
+#define EMIT(x) void emit_##x(struct x##_s * n)
+
+EMIT(specifier_list);
+
+EMIT(subrule_list) {
+    if (n->subrule_list) {
+	emit_subrule_list(n->subrule_list);
+	printf(";\n");
+    }
+    emit_specifier_list(n->specifier_list);
+}
+
+EMIT(chaingroup_specifier) {
+    printf("[ ");
+    if (n->name)
+	printf("\"%s\" ", n->name);
+    printf("\n");
+    emit_subrule_list(n->list);
+    printf("]\n");
+}
+
+EMIT(compound_specifier) {
+    printf("{\n");
+    emit_subrule_list(n->list);
+    printf("}\n");
+}
+
+EMIT(routing_specifier) {
+    /* print type */
+}
+
+EMIT(simple_icmptype_argument) {
+    printf("%s", n->identifier);
+}
+
+EMIT(icmptype_argument_list) {
+    if (n->list) {
+	emit_icmptype_argument_list(n->list);
+	printf(" ");
+    }
+    emit_simple_icmptype_argument(n->arg);
+}
+
+EMIT(compound_icmptype_argument) {
+    printf("{ ");
+    emit_icmptype_argument_list(n->list);
+    printf(" }");
+};
+
+EMIT(icmptype_argument) {
+    if (n->compound)
+	emit_compound_icmptype_argument(n->compound);
+    else
+	emit_simple_icmptype_argument(n->simple);
+}
+
+EMIT(icmptype_specifier) {
+    printf("icmptype ");
+    emit_icmptype_argument(n->arg);
+}
+
+EMIT(simple_protocol_argument) {
+    printf("%s ", n->identifier);
+}
+
+EMIT(protocol_argument_list) {
+    if (n->list) {
+	emit_protocol_argument_list(n->list);
+	printf(" ");
+    }
+    emit_simple_protocol_argument(n->arg);
+}
+
+EMIT(compound_protocol_argument) {
+    printf("{ ");
+    emit_protocol_argument_list(n->list);
+    printf(" }");
+}
+
+EMIT(protocol_argument) {
+    if (n->compound)
+	emit_compound_protocol_argument(n->compound);
+    else
+	emit_simple_protocol_argument(n->simple);
+}
+
+EMIT(protocol_specifier) {
+    /* print type */
+    emit_protocol_argument(n->arg);
+}
+
+EMIT(simple_port_argument) {
+    printf("%d", n->port_min);
+    /* FIX this */
+    printf(":%d", n->port_max);
+}
+
+EMIT(port_argument_list) {
+    if (n->list)
+	emit_port_argument_list(n->list);
+    emit_simple_port_argument(n->arg);
+}
+
+EMIT(compound_port_argument) {
+    printf("{ ");
+    emit_port_argument_list(n->list);
+    printf(" }");
+}
+
+EMIT(port_argument) {
+    if (n->compound)
+	emit_compound_port_argument(n->compound);
+    else
+	emit_simple_port_argument(n->simple);
+}
+
+EMIT(port_specifier) {
+    /* print type */
+    emit_port_argument(n->arg);
+}
+
+EMIT(simple_host_argument) {
+    printf("%s", n->host);
+}
+
+EMIT(host_argument_list) {
+    if (n->list) {
+	emit_host_argument_list(n->list);
+	printf(" ");
+    }
+    emit_simple_host_argument(n->arg);
+}
+
+EMIT(compound_host_argument) {
+    printf("{ ");
+    emit_host_argument_list(n->list);
+    printf(" }");
+}
+
+EMIT(host_argument) {
+    if (n->compound)
+	emit_compound_host_argument(n->compound);
+    else
+	emit_simple_host_argument(n->simple);
+}
+
+EMIT(host_specifier) {
+    /* print type */
+    emit_host_argument(n->arg);
+}
+
+EMIT(log_text_argument) {
+    printf("text \"%s\" ", n->text);
+}
+
+EMIT(log_target_specifier) {
+    printf("log ");
+    if (n->arg)
+	emit_log_text_argument(n->arg);
+}
+
+EMIT(target_specifier) {
+    /* print type */
+}
+
+EMIT(simple_direction_argument) {
+    printf("%s ", n->identifier);
+}
+
+EMIT(direction_argument_list) {
+    if (n->list) {
+	emit_direction_argument_list(n->list);
+	printf(" ");
+    }
+    emit_simple_direction_argument(n->arg);
+}
+
+EMIT(compound_direction_argument) {
+    printf("{ ");
+    emit_direction_argument_list(n->list);
+    printf(" }");
+}
+
+EMIT(direction_argument) {
+    if (n->compound)
+	emit_compound_direction_argument(n->compound);
+    else
+	emit_simple_direction_argument(n->simple);
+}
+
+EMIT(direction_specifier) {
+    /* print type */
+    emit_direction_argument(n->arg);
+}
+
+EMIT(specifier) {
+    if (n->compound)
+	emit_compound_specifier(n->compound);
+    else if (n->direction)
+	emit_direction_specifier(n->direction);
+    else if (n->target)
+	emit_target_specifier(n->target);
+    else if (n->host)
+	emit_host_specifier(n->host);
+    else if (n->port)
+	emit_port_specifier(n->port);
+    else if (n->protocol)
+	emit_protocol_specifier(n->protocol);
+    else if (n->icmptype)
+	emit_icmptype_specifier(n->icmptype);
+    else if (n->routing)
+	emit_routing_specifier(n->routing);
+    else
+	emit_chaingroup_specifier(n->chaingroup);
+}
+
+EMIT(negated_specifier) {
+    /* print type */
+    emit_specifier(n->spec);
+}
+
+EMIT(specifier_list) {
+    if (n->list) {
+	emit_specifier_list(n->list);
+	/* specifiers are separated by spaces */
+	printf(" ");
+    }
+    emit_negated_specifier(n->spec);
+}
+
+EMIT(rule) {
+    if (n->list)
+	emit_specifier_list(n->list);
     /* rules end in a semicolon and newline */
     printf(";\n");
 }
 
-void emit_rule_list(struct rule_list_s * rule_list) {
-    if (rule_list->rule_list) {
-	emit_rule_list(rule_list->rule_list);
+EMIT(rule_list) {
+    if (n->list) {
+	emit_rule_list(n->list);
     }
-    if (rule_list->rule) {
-	emit_rule(rule_list->rule);
+    if (n->rule) {
+	emit_rule(n->rule);
     }
-};
+}
 
-void emit_ast(struct ast_s * ast) {
-    if (ast->rule_list)
-	emit_rule_list(ast->rule_list);
-};
+EMIT(ast) {
+    if (n->list)
+	emit_rule_list(n->list);
+}
 
 int main(int argc, char ** argv) {
     struct ast_s ast;
