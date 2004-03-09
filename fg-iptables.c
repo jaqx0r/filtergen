9d26bfaf1afab21f66c2c1279d98f6a8cec59dc0
Jamie Wilkinson <jaq@spacepants.org>
2004-03-09 06:38:18 +0000

    cscvs to tla changeset 1 Author: matthew Date: 2001/09/25 17:22:39 Initial checkin of filter compiler.

---
+++ fg-iptables.c
@@ -0,0 +1,88 @@
+/*
+ * Filter generator, iptables driver
+ *
+ * $Id: fg-iptables.c,v 1.1 2001/09/25 17:22:39 matthew Exp $
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "filter.h"
+
+int fg_iptables(const struct filterent *ent, void *misc)
+{
+	const char *chain, *chain_r, *target;
+	const char *proto, *state, *state_r;
+	const char *ifarg, *ifarg_r;
+	int needret = 0;
+
+	/* this should all be put in a table somehow */
+
+	switch(ent->direction) {
+	case F_INPUT:	chain = "input"; chain_r = "output";
+			ifarg = "-i"; ifarg_r = "-o"; break;
+	case F_OUTPUT:	chain = "output"; chain_r = "input";
+			ifarg = "-o"; ifarg_r = "-i"; break;
+	default: fprintf(stderr, "unknown direction\n"); abort();
+	}
+
+	if(!(ent->iface)) ifarg = ifarg_r = NULL;
+
+	/* state and reverse rules here */
+	switch(ent->proto) {
+	case 0:
+		proto = NULL;
+		state = state_r = NULL;
+		break;
+	case TCP:
+		needret++;
+		proto = "tcp";
+		state = "--state=NEW,ESTABLISHED";
+		state_r = "--state=ESTABLISHED ! -y";
+		break;
+	case UDP:
+		needret++;
+		proto = "udp";
+		state = "--state=NEW,ESTABLISHED";
+		state_r = "--state=ESTABLISHED";
+		break;
+	default: abort();
+	}
+
+#define	NL(s) ((s) ?: "")
+#define ARGIF(v,s) ((v) ? " " s " " : "")
+#define ARGSIF(v,s) ARGIF(v,s), NL(v)
+#define NARGSIF(a,v,s) ((ent->whats_negated & (1<<F_##a)) ? " !" : ""),	\
+			ARGSIF(v,s)
+
+	switch(ent->target) {
+	case F_ACCEPT: target = "ACCEPT"; break;
+	case F_DROP: target = "DROP"; break;
+	case F_REJECT: needret = 0; state = NULL; target = "REJECT"; break;
+	default: abort();
+	}
+
+#define FORMAT "iptables -A %s %s %s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s %s -J %s\n"
+	printf(FORMAT,
+			chain,
+			NL(ifarg), NL(ent->iface),
+			NARGSIF(PROTO, proto, "-p"),
+			NARGSIF(SOURCE, ent->srcaddr, "-s"),
+			NARGSIF(DEST, ent->dstaddr, "-d"),
+			NARGSIF(SPORT, ent->u.ports.src, "--sport"),
+			NARGSIF(DPORT, ent->u.ports.dst, "--dport"),
+			NL(state), target);
+	if(needret) {
+		printf(FORMAT,
+			chain_r,
+			NL(ifarg_r), NL(ent->iface),
+			NARGSIF(PROTO, proto, "-p"),
+			NARGSIF(SOURCE, ent->srcaddr, "-d"),
+			NARGSIF(DEST, ent->dstaddr, "-s"),
+			NARGSIF(SPORT, ent->u.ports.src, "--dport"),
+			NARGSIF(DPORT, ent->u.ports.dst, "--sport"),
+			NL(state_r), target);
+	}
+
+	return 1 + !!needret;
+}
---
+++ filter.c
@@ -0,0 +1,160 @@
+/* $Id: filter.c,v 1.1 2001/09/25 17:22:39 matthew Exp $ */
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "filter.h"
+
+
+struct filter *__new_filter(enum filtertype type)
+{
+	struct filter *f;
+	if ((f = calloc(1, sizeof(*f)))) {
+		f->type = type;
+	}
+	return f;
+}
+
+
+struct filter *new_filter_neg(struct filter *sub)
+{
+	struct filter *f;
+	if ((f = __new_filter(F_NEG))) {
+		f->u.neg = sub;
+	}
+	return f;
+}
+
+struct filter *new_filter_sibs(struct filter *list)
+{
+	struct filter *f;
+//	if(!list || !list->next) return list;
+	if ((f = __new_filter(F_SIBLIST))) {
+		f->u.sib = list;
+	}
+	return f;
+}
+
+
+
+struct filter *new_filter_proto(enum filtertype proto)
+{
+	struct filter *f;
+	if ((f = __new_filter(F_PROTO))) {
+		f->u.proto = proto;
+	}
+	return f;
+}
+
+struct filter *new_filter_device(enum filtertype type, const char *iface)
+{
+	struct filter *f;
+	if ((f = __new_filter(type))) {
+		f->u.iface = strdup(iface);
+	}
+	return f;
+}
+
+
+struct filter *new_filter_host(enum filtertype type, const char *matchstr)
+{
+	struct filter *f;
+	if ((f = __new_filter(type))) {
+		f->u.addrs = strdup(matchstr);
+	}
+	return f;
+}
+
+
+struct filter *new_filter_ports(enum filtertype type, const char *matchstr)
+{
+	struct filter *f;
+	if ((f = __new_filter(type))) {
+		f->u.ports = strdup(matchstr);
+	}
+	return f;
+}
+
+
+/*
+ * These functions DAGify the parsed filter structure.
+ * This means that we can walk all paths down the DAG
+ * merely by following ->child and ->next.
+ */
+
+static void filter_append(struct filter *f, struct filter *x)
+{
+	if(!f) abort();
+	while((f->type != F_SIBLIST) && f->child)
+		f = f->child;
+
+	if(f->type == F_SIBLIST) {
+		if(f->child) abort();
+		for(f=f->u.sib;f;f=f->next)
+			filter_append(f,x);
+	} else
+		f->child = x;
+}
+
+/*
+ * The easy bit of a cross-product.  Basically we ensure that no
+ * filter node has more than one path out.
+ * 1. We push sibling->child down to the end of the component
+ *    sub-lists, and
+ * 2. Ensure that negated entries have only a single component
+ *    hanging off them.
+ */
+void __filter_unroll(struct filter *f)
+{
+	struct filter *c, *s;
+
+	if(!f) return;
+
+	/* depth first */
+	__filter_unroll(c = f->child);
+
+	/* check this node */
+	switch(f->type) {
+	case F_SIBLIST:
+		for(s = f->u.sib; s; s = s->next) {
+			__filter_unroll(s);
+			filter_append(s, c);
+		}
+		f->child = NULL;
+		break;
+	case F_NEG: abort();
+	default: break;
+	}
+
+	/* lastly, go sideways */
+	__filter_unroll(f->next);
+}
+
+
+void __filter_neg_expand(struct filter **f, int neg)
+{
+	if(!*f) return;
+	__filter_neg_expand(&(*f)->child, neg);
+	__filter_neg_expand(&(*f)->next, neg);
+
+	switch((*f)->type) {
+	case F_SIBLIST:	__filter_neg_expand(&(*f)->u.sib, neg); break;
+	case F_NEG: {
+		struct filter *c = (*f)->child;
+		*f = (*f)->u.neg;
+		neg = !neg;
+		__filter_neg_expand(f, neg);
+		if(c) filter_append(*f, c);
+		break;
+	}
+	default: break;
+	}
+	(*f)->negate = neg;
+}
+
+
+void filter_unroll(struct filter **f)
+{
+	__filter_neg_expand(f, 0);
+	__filter_unroll(*f);
+}
