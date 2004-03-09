3bcb8b8186a678bc63e203cf82d20e4c62a0ee46
Jamie Wilkinson <jaq@spacepants.org>
2004-03-09 06:39:38 +0000

    cscvs to tla changeset 60 Author: matthew Date: 2002/07/18 21:28:04 Implement "local" for ipchains driver

--- fg-ipchains.c
+++ fg-ipchains.c
@@ -3,7 +3,7 @@
  *
  * XXX - maybe some of this could be shared with the iptables one?
  *
- * $Id: fg-ipchains.c,v 1.14 2002/07/18 21:23:19 matthew Exp $
+ * $Id: fg-ipchains.c,v 1.15 2002/07/18 21:28:04 matthew Exp $
  */
 
 #include <stdio.h>
@@ -18,6 +18,7 @@ static int cb_ipchains_rule(const struct filterent *ent, struct fg_misc *misc)
 	char *rule = NULL, *rule_r = NULL;
 	char *rulechain = NULL, *revchain = NULL;
 	int needret = 0;
+	int isforward = (ent->rtype != LOCALONLY);
 
 	/* nat rule */
 	if((ent->target == F_MASQ) || (ent->target == F_REDIRECT)) {
@@ -124,6 +125,10 @@ static int cb_ipchains_rule(const struct filterent *ent, struct fg_misc *misc)
 
 	oprintf("ipchains -A %s %s\n", rulechain, rule+1);
 	if(needret) oprintf("ipchains -A %s %s\n", revchain, rule_r+1);
+	if(isforward) {
+		oprintf("ipchains -A FORWARD %s\n", rule+1);
+		if(needret) oprintf("ipchains -A FORWARD %s\n", rule_r+1);
+	}
 
 	free(rule); free(rule_r);
 	return 1 + !!needret;
