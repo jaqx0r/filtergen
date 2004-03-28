978fd66719e704fcc2e2690df05f064a393672b5
Jamie Wilkinson <jaq@spacepants.org>
2004-03-28 04:03:32 +0000

    added more notes More notes written about generating target rules and factorising. factoriser.(c|h) added to the build, even though they're no-ops. started to flesh out expression structures in expr.h

---
+++ expr.h
@@ -0,0 +1,39 @@
+#ifndef __EXPR_H__
+#define __EXPR_H__
+
+enum expression = {
+    EXP_AND,
+    EXP_OR,
+    EXP_NOT,
+    EXP_PRED,
+};
+
+struct expr_s * {
+    enum expression type;
+    struct expr_s * 
+    enum expression ex;
+    struct expr_s * e1;
+    struct expr_s * e2;
+};
+
+enum action = {
+    ACT_NONE,
+    ACT_ACCEPT,
+    ACT_REJECT,
+    ACT_DROP,
+};
+
+/* rule is a boolean expression of filtering predicates -> action */
+struct rule_s {
+    struct expr_s * expr;
+    enum action act;
+    struct rule_s * next;
+};
+
+struct group_s {
+    char * name;
+    struct rule_s * rule_list;
+};
+    
+
+#endif /* __EXPR_H__ */
