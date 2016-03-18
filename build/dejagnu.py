2510a347af0adacd07681f3663fc94fb52acd734
Jamie Wilkinson <jaq@spacepants.org>
2016-03-18 20:55:22 +1100

    Add a dejagnu builder for SCons to run the tests.

---
+++ build/dejagnu.py
@@ -0,0 +1,29 @@
+import re
+import os
+import os.path
+import glob
+
+
+def dejagnuAction(target, source, env):
+    if env['with_runtest'] == None:
+        print "Testing disabled because 'runtest' not found"
+        return 0
+
+    context = os.path.basename(env.File(target[0]).path)
+    context = re.sub('(.*?)\..*', '\\1', context)
+    os.system('cd ' + os.path.join(env['BuildDir'], 'test') +
+              '; DEJAGNU="' +
+              os.path.join(env['AbsObjRoot'], 'test', 'site.exp') + '" ' +
+              env['with_runtest'] + ' --tool ' + context)
+    return 0
+
+
+def dejagnuMessage(target, source, env):
+    return 'Running DejaGNU Test Suite'
+
+
+def DejaGnu(env, dirs):
+    dgAction = env.Action(dejagnuAction, dejagnuMessage)
+    dgBuilder = env.Builder(action=dgAction, suffix='results')
+    env.Append(BUILDERS={'DejaGnu': dgBuilder})
+    return 1
