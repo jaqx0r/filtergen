796bb0961c65e02658f570a782a41dde1823fa78
Jamie Wilkinson <jaq@spacepants.org>
2024-08-22 16:42:46 +1000

    Call the `oputs` C function from the flush implementation.
    Using FFI, but also create a method to call that does the C marshalling for us.

--- src/output/nftables.rs
+++ src/output/nftables.rs
@@ -1,3 +1,6 @@
+use std::os::raw::c_char;
+use std::ffi::CString;
+
 #[repr(C)]
 pub struct Filter {
 }
@@ -36,6 +39,21 @@ pub enum FilterType {
     Text,
 }
 
+extern {
+    /// Output a string, and appends a newline, to the target file.
+    /// The target file is a global in the called object.
+    fn oputs(s: *const c_char);
+}
+
+fn write(s: &str) {
+    let c_str = CString::new(s).expect("c string new failed");
+    unsafe {
+        oputs(c_str.as_ptr());
+    }
+}
+
+
+
 #[no_mangle]
 pub extern "C" fn fg_nftables(_filter: *const Filter, _flags: u32) -> i32 {
     0
@@ -44,5 +62,7 @@ pub extern "C" fn fg_nftables(_filter: *const Filter, _flags: u32) -> i32 {
 
 #[no_mangle]
 pub extern "C" fn flush_nftables(_policy: FilterType) -> i32 {
+    write("nft");
+    
     0
 }
