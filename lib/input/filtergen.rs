1c1f305d3adb9810455eebbcd43264bc65ce083a
Jamie Wilkinson <jaq@spacepants.org>
2024-10-19 16:03:59 +1100

    Build the tree structure of the parser based only on filter options.

--- lib/input/filtergen.rs
+++ lib/input/filtergen.rs
@@ -1,10 +1,11 @@
 use nom::{
     branch::alt,
     bytes::complete::tag,
-    character::complete::{alpha1, multispace0},
+    character::complete::{alpha1, char, multispace0},
     combinator::{map, opt, value},
     error::VerboseError,
-    sequence::{pair, preceded},
+    multi::many0,
+    sequence::{pair, preceded, terminated},
     IResult, Parser,
 };
 
@@ -62,4 +63,77 @@ fn parse_option_test() {
         parse_option("log text"),
         Ok(("", FilterOption::Log(Some("text".to_string()))))
     );
+}
+
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub enum Specifier {
+    FilterOption(FilterOption),
+    Negated(Box<Specifier>),
+}
+
+fn parse_specifier(input: &str) -> IResult<&str, Specifier, VerboseError<&str>> {
+    alt((
+        map(parse_option, |o| Specifier::FilterOption(o)),
+        map(
+            preceded(pair(tag("!"), multispace0), parse_specifier),
+            |s| Specifier::Negated(Box::new(s)),
+        ),
+    ))
+    .parse(input)
+}
+
+#[test]
+fn parse_specifier_test() {
+    assert_eq!(
+        parse_specifier("local"),
+        Ok(("", Specifier::FilterOption(FilterOption::Local)))
+    );
+    assert_eq!(
+        parse_specifier("!local"),
+        Ok((
+            "",
+            Specifier::Negated(Box::new(Specifier::FilterOption(FilterOption::Local)))
+        ))
+    );
+}
+
+fn parse_rule(input: &str) -> IResult<&str, Vec<Specifier>, VerboseError<&str>> {
+    terminated(many0(preceded(multispace0, parse_specifier)), char(';')).parse(input)
+}
+
+#[test]
+fn parse_rule_test() {
+    assert_eq!(
+        parse_rule("local forward !local;"),
+        Ok((
+            "",
+            vec![
+                Specifier::FilterOption(FilterOption::Local),
+                Specifier::FilterOption(FilterOption::Forward),
+                Specifier::Negated(Box::new(Specifier::FilterOption(FilterOption::Local))),
+            ]
+        ))
+    );
+}
+
+pub fn parse(input: &str) -> IResult<&str, Vec<Vec<Specifier>>, VerboseError<&str>> {
+    many0(preceded(multispace0, parse_rule)).parse(input)
+}
+
+#[test]
+fn parse_test() {
+    assert_eq!(
+        parse("local forward;
+forward !local;"),
+        Ok(("",
+        vec![
+            vec![
+                Specifier::FilterOption(FilterOption::Local),
+                Specifier::FilterOption(FilterOption::Forward),
+            ],
+            vec![
+                Specifier::FilterOption(FilterOption::Forward),
+                Specifier::Negated(Box::new(Specifier::FilterOption(FilterOption::Local))),
+            ],
+        ])))
 }
